7b75f64f018389bd953fd89edf3abf4ddff0ff88
Arseny Chekanov <senya@chekanov.net>
2017-12-16 18:54:45 +0300

    Add Human class

--- js/init.js
+++ js/init.js
@@ -1,3 +1,18 @@
+function Human(x, y, health) {
+    this.x = x;
+    this.y = y;
+    this.health = health;
+}
+
+Human.prototype.dealDamage = function(damage) {
+    this.health -= damage;
+};
+
+Human.prototype.isDead = function() {
+    return this.health <= 0;
+};
+
+
 function init() {
     var stage = new createjs.Stage("demoCanvas");
     var circle = new createjs.Shape();
@@ -6,4 +21,14 @@ function init() {
     circle.y = 100;
     stage.addChild(circle);
     stage.update();
+
+    human = new Human(200, 200, 100);
+
+    human.dealDamage(50);
+    console.log(human.health);
+    console.log(human.isDead());
+
+    human.dealDamage(50);
+    console.log(human.health);
+    console.log(human.isDead());
 }
